e1f552c65d8f90000ee4f06213ee3ce443baed85
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2023-01-16 11:07:19 +0100

    ENV_FILE + CONTAINER_NAME

--- run-wopee.sh
+++ run-wopee.sh
@@ -6,6 +6,12 @@ if [ -z ${IMAGE+x} ]; then
 fi
 echo "IMAGE: $IMAGE"
 
+if [ -z ${CONTAINER_NAME+x} ]; then
+    echo "Warning: CONTAINER_NAME not set, using default"
+    export CONTAINER_NAME=wopee-runner
+fi
+echo "CONTAINER_NAME: $CONTAINER_NAME"
+
 if [ -z ${CONFIG+x} ]; then
     echo "Warning: CONFIG not set, using default"
     export CONFIG=config.yaml
@@ -13,7 +19,7 @@ fi
 echo "CONFIG: $CONFIG"
 
 if [ -f $CONFIG ]; then
-    echo "File '$CONFIG' does exist localy and will be used."
+    echo "File '$CONFIG' exists localy and will be used."
     export LOCAL_CONFIG_MOUNT=$(pwd)/$CONFIG
     export CONTAINER_CONFIG_MOUNT=/home/wopee/$CONFIG
 else
@@ -22,17 +28,34 @@ else
     export CONTAINER_CONFIG_MOUNT=/home/wopee/pwd
 fi
 
+if [ -f $ENV_FILE ]; then
+    echo "File '$ENV_FILE' exists localy and will be used."
+else
+    if [ -z ${ENV_FILE+x} ]; then
+        echo "Info: ENV_FILE not set."
+    else 
+        echo "Warning: File '$ENV_FILE' does not exist."
+    fi
+    echo "ENV_FILE_NOT_SET=true" > .env_file_not_set.env
+    export ENV_FILE=.env_file_not_set.env
+fi
+echo "ENV_FILE: $ENV_FILE"
+
 if [ -z ${SECCOMP_PROFILE+x} ]; then
     echo "Warning: SECCOMP_PROFILE not set, using default"
     export SECCOMP_PROFILE=seccomp_profile.json
 fi
 echo "SECCOMP_PROFILE: $SECCOMP_PROFILE"
 
+docker ps -q --filter "name=$CONTAINER_NAME" | grep -q . && docker stop $CONTAINER_NAME
+
 docker pull ${IMAGE}
 
 docker run --rm \
+    --name $CONTAINER_NAME \
     -v $LOCAL_CONFIG_MOUNT:$CONTAINER_CONFIG_MOUNT:ro \
     --env-file <(env | sed '/^PATH=/d;/^HOME=/d;/^USER=/d;/^_=/d') \
+    --env-file $ENV_FILE \
     --ipc=host \
     --network=host \
     --security-opt seccomp=$SECCOMP_PROFILE \
